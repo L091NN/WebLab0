70ad7fed8195c4699b1adc0e2f1e358fb83ad8a7
Михаил Лугин <329898@mail.ru>
2019-11-09 11:59:58 +0300

    Add sketch lab0

---
+++ jsCode.js
@@ -0,0 +1,8 @@
+function hello() {alert("Привет! ");}
+function ClickedButton1()
+{
+	document.getElementById('button2').style.color = 'blue'
+
+	let input = document.createElement('input');
+	document.body.form1.append(input);
+}
